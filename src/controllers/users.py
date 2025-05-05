2360260d3e5e9be059bd53ea6b2a692875c95f89
Lily_Chou <98002855+hellowhaaa@users.noreply.github.com>
2025-05-05 15:43:41 +0800

    feat: add upload-csv endpoint to create users from csv file and add average age calculation endpoint

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,9 +1,10 @@
 
-from fastapi import APIRouter, Depends
-from schemas.users import UserRequest, UserResponse
+from fastapi import APIRouter, Depends, UploadFile
+from schemas.users import UserRequest, UserResponse, AverageAgeResponse
 from services.users import UserService
 from sqlalchemy.orm import Session
 from core.database import get_db
+from io import StringIO
 
 router = APIRouter()
 
@@ -32,5 +33,20 @@ def list_users(db: Session = Depends(get_db)):
     List all users.
     """
     response = UserService(db).get_all_users()
+    return response
     
+@router.post("/upload-csv/")
+def upload_csv(file: UploadFile, db: Session = Depends(get_db)):
+    """
+    Upload and parse a CSV file.
+    """
+    resp = UserService(db).create_user_via_csv(file)
+    return resp
+
+@router.get("/average-age", response_model=list[AverageAgeResponse])
+def list_users(db: Session = Depends(get_db)):
+    """
+    Get all users' average ages.
+    """
+    response = UserService(db).get_average_age_grouped_by_first_char()
     return response
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -8,3 +8,7 @@ class UserRequest(BaseModel):
 class UserResponse(BaseModel):
     name: str = Field(..., description="The name of the user")
     age: int = Field(..., description="The age of the user")
+    
+class AverageAgeResponse(BaseModel):
+    firstCharacter: str = Field(..., description="the first character of the usernames")
+    age: int = Field(..., description="The average age of same first character username")
